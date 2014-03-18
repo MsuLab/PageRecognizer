27eef20c94d574508f0aea560a56931985ad7c00
Boris Kopin <kopbob@gmail.com>
2014-03-18 23:44:48 +0400

    Separate TImage class.

--- core.py
+++ core.py
@@ -5,74 +5,75 @@ from optparse import *
 import numpy as np
 import pylab as plt
 
-class TImage:
+import os.path
 
-    def __init__(self, imagePath):
 
-        self.__image = imread( imagePath, CV_LOAD_IMAGE_GRAYSCALE )
+class ImageBase(object):
+    img = None
+    __ref_img = None
+
+    window_height = 800.0
+
+
+    def __init__(self, path_to_img):
+        super(ImageBase, self).__init__()
+
+        img = imread(path_to_img, CV_LOAD_IMAGE_GRAYSCALE)
 
         try:
-            if not self.__image.all():
+            if not img.all():
                 raise IOError("No image data \n")
         except AttributeError:
             raise IOError("No image data \n")
 
-        self.__channels = lambda: 1 if len(self.__image.shape) == 2  else self.image.shape[2]
+        self.img = img
+        self.__ref_img = img
+        self.channels = lambda: 1 if len(self.img.shape) == 2 else self.img.shape[2]
 
-    def render(self, window_name) :
-        # Define height for showing
-        h = 500.0
-        scale = h / self.__image.shape[0]
+    def render(self, window_name="main", img = None):
+        if not img:
+            img = self.img
+
+        scale = self.window_height / img.shape[0]
+
         destroyAllWindows()
-        namedWindow(window_name)#, WINDOW_NORMAL)
-        imshow(window_name, resize(self.__image, None, fy=scale, fx=scale))
-        #resizeWindow(window_name, 500, 500)
-        print  "Render" 
+        namedWindow(window_name)
+        imshow(window_name, resize(img, None, fx=0.3, fy=0.3, interpolation=INTER_CUBIC))
         waitKey(0)
-    
+
+
+class PreprocessImage(ImageBase):
+
+    __threshold = 150
+
+    def __init__(self, path_to_img):
+        super(PreprocessImage, self).__init__(path_to_img)
+
+        self.apply_blur()
+        self.apply_threshold()
+        self.apply_denoise()
+        self.apply_morphologyEx()
 
     def apply_adaptiveThreshold(self) :
         print  "Apply adaptiveThreshold ... "
-        self.__image = adaptiveThreshold(self.__image, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 15, 10)
+        self.img = adaptiveThreshold(self.img, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 15, 10)
 
-        self.render("adaptiveThreshold")
-    
+    def apply_blur(self):
+        print  "Apply blur ... "
+        self.img = blur(self.img, (3,3))
 
     def apply_threshold(self) :
         print  "Apply threshold ... "
-        retval, self.__image = threshold( self.__image, 100, 255, THRESH_BINARY_INV | THRESH_OTSU )
-        #threshold( self.__image, self.__image, 100, 255, THRESH_OTSU )
-
-        self.render("threshold")
-    
-
-    def apply_morphologyEx(self) :
-        print  "Apply morphologyEx ... "
-        element = getStructuringElement( MORPH_CROSS, (3, 3), (1, 1))
-        self.__image = morphologyEx(self.__image,  MORPH_CLOSE, element)
-
-        self.render("morphologyEx")
-    
-
-    def apply_fastNlMeansDenoising(self) :
-        """No such function in Python"""
-#        print  "Use fastNlMeansDenoising ... "
-#
-#        self.__image = fastNlMeansDenoising(self.__image,
-#                            3,# float h=3,
-#                            7,# int templateWindowSize=7,
-#                            22# int searchWindowSize=21 
-#                    )
-#        self.render("fastNlMeansDenoising")
-    
+        retval, self.img = threshold( self.img, self.__threshold, 255, THRESH_BINARY_INV | THRESH_OTSU )
+        #threshold( self.img, self.img, 100, 255, THRESH_OTSU )
 
     def apply_denoise(self):
         print  "Apply denoise ... "
-        (contours, hierarchy) =  findContours( self.__image, RETR_CCOMP, CHAIN_APPROX_NONE )
+        (contours, hierarchy) =  findContours( self.img, RETR_CCOMP, CHAIN_APPROX_NONE )
 
-        self.__image = np.ones(self.__image.shape, np.uint8) * 255
-        color_parent = [0] * self.__channels()
-        color_child = [255] * self.__channels()
+        self.img = np.ones(self.img.shape, np.uint8) * 255
+        color_parent = [0] * self.channels()
+        color_child = [255] * self.channels()
         for i in range(len(contours)):
             area = contourArea(contours[i])
             if area > 5 and area < 10000:
@@ -83,75 +84,37 @@ class TImage:
                     color = color_child
 
                 #color = Scalar( rand()&255, rand()&255, rand()&255 )
-                #drawContours( self.__image, contours, i, color, CV_FILLED, 8, hierarchy, 0, Point() )
-                drawContours( self.__image, contours, i, color, -1, 8, hierarchy, 0, None )
+                #drawContours( self.img, contours, i, color, CV_FILLED, 8, hierarchy, 0, Point() )
+                drawContours( self.img, contours, i, color, -1, 8, hierarchy, 0, None )
 
-        self.render("Denoised")
+    def apply_morphologyEx(self) :
+        print  "Apply morphologyEx ... "
+        element = getStructuringElement( MORPH_CROSS, (3, 3), (1, 1))
+        self.img = morphologyEx(self.img,  MORPH_CLOSE, element)
     
-
-    def drawRectOnImage(self,rect = np.array([40, 40, 30, 30])) :
-        print  "Draw roi ... "
-        pt1 = rect[:2] 
-        pt2 = rect[:2] + rect[2:] 
-        rectangle(self.__image, tuple(pt1 + np.array([-1, -1])), tuple(pt2 + np.array([1, 1])), [0] * self.__channels())
-        rectangle(self.__image, tuple(pt1 + np.array([-3, -3])), tuple(pt2 + np.array([3, 3])), [0] * self.__channels())
-        self.render("New rectangle")
-    
-
-    def getRoi(self,rect = np.array([40, 40, 30, 30])) :
-        #Rect roi((int)(self.__image.size().width/2) - 10, 4, w, h)
-        image_roi = self.__image[rect[0]:rect[0]+rect[2], rect[1]:rect[1]+rect[3]]
-        return image_roi
-    
-
-    def getHist(self, roi, display = False):
-        print  "Calculating histogram ... "
-
-        hist, bin_edges  = plt.histogram(roi.reshape(roi.size), bins=2)
-
-        if display :
-            plt.bar(bin_edges[:-1], hist, width = 127)
-            plt.xlim(np.min(bin_edges), np.max(bin_edges))
-            plt.show()
-            # add a line showing the expected distribution
-            
-            #waitKey(0)
-
-        return float (hist[0]) / sum(hist)
-
 
 def parse_input():
-    # Parse input options
-    usage = "usage: %prog <Image_Path>"
-    parser=OptionParser(usage)
-    #parser.add_option('-p','--plot', type='int', default=True, help="Matplotlib")
+    usage = "usage: %prog [options] arg1"
+    parser = OptionParser(usage)
+    parser.add_option("-i", "--image", dest="path_to_image",
+                  help="Path to image.", metavar="path_to_image")
+
     (options, args) = parser.parse_args()
 
-    if not args or len(args) > 1:
-        raise IOError("usage: DisplayImage.py <Image_Path>\n")
-    return args[0]
+    if (options.path_to_image is None):
+        parser.print_help()
+        exit(-1)
+    elif not os.path.exists(options.path_to_image):
+        parser.error('image does not exists')
 
-def main():
+    return options
 
-    try:
-
-        imagePath = parse_input()
-        timage = TImage(imagePath)
-
-        timage.render("Image")
-        timage.apply_threshold()
-        #timage.apply_morphologyEx()
-        timage.apply_denoise()
-
-        rect = np.array([300, 100, 200, 200])
-        timage.drawRectOnImage(rect)
-        hist = timage.getHist(timage.getRoi(rect))#,True)
-        print  "Black percentage: %s %%" % ( hist * 100) 
-
-    except IOError as e:
-        print "\nException raised: %s " % (e)
 
 
 if __name__ == '__main__':
-    main()
+    opt = parse_input()
+
+    timage = PreprocessImage(opt.path_to_image)
+    timage.render()
+
 
