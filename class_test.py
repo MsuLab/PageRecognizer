4a29393635c268f9baf2bb5c3ea9fc8bc8d2645f
Vladimir-Kozlov <kozlov2.volodia2@gmail.com>
2014-07-04 16:49:23 +0400

    Now images are being turned multiple angles for better recognition.

--- class_test.py
+++ class_test.py
@@ -4,6 +4,7 @@
 import os.path
 import optparse
 import cv2
+import numpy
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw 
@@ -13,26 +14,44 @@ from preco.TImage import TImage
 
 from classifier import classify
 
+from digit_binarization import apply_filters
+
 
 if __name__ == '__main__':
-    files_list = ['1-2.jpg', '3-1.jpg', '5-1.jpg', '5-2.jpg', '7-1.jpg', '8-1.jpg', '8-2.jpg']
+    files_list = ['1.png', '2.png', '3.png', '4.png'] #'1.png', '2.png', '3.png',
+    
     for file in files_list:
         timage = TImage(file)
         cv2.imshow('image', timage.img)
         cv2.waitKey(0)
-        #img = timage.img
-        #img = cv2.resize(timage.img, (28, 28));
-        z = [px / 255.0 for px in cv2.resize(timage.img, (28, 28)).flatten('C').tolist()]
-        probs = classify(z)
-        max_value = max(probs)
-        #print probs
+        img_filtered = cv2.resize(apply_filters(timage.img), (28, 28))
+        max_value = range(10)
+        index = range(10)
+        for angle in xrange(-10, 50, 5):
+            #print img_filtered.shape
+            rot_mat = cv2.getRotationMatrix2D((img_filtered.shape[0]/2, img_filtered.shape[1]/2), angle, 1)
+            img_rotated = cv2.warpAffine(img_filtered, rot_mat, (img_filtered.shape[0], img_filtered.shape[1]),flags=cv2.INTER_LINEAR)
+            #print img_rotated.shape
+            img_scaled = cv2.resize(img_rotated, (28, 28))
+            cv2.imshow('scale', img_rotated)
+            cv2.waitKey(0)
+            z = [px / 255.0 for px in img_scaled.flatten('C').tolist()]
+            #print z
+            probs = classify(z)
+            #print probs
+            max_value[angle / 5] = max(probs)
+            if max_value[angle / 5] < 0.5:
+                index[angle / 5] = -1
+            else:
+                index[angle / 5] = probs.index(max_value[angle / 5])
         img = Image.open(file)
         draw = ImageDraw.Draw(img)
         
-        if max_value < 0.5:
+        max_value_overall = max(max_value)
+        if max_value_overall < 0.5:
             s = 'ERROR'
         else:
-            s = 'DIGIT: %i' % probs.index(max_value)
+            s = 'DIGIT: %i' % index[max_value.index(max_value_overall)]
         print s
         font = ImageFont.truetype("ARIAL.TTF", 20)
         draw.text((0, 0), s, (0, 255, 0), font=font)
